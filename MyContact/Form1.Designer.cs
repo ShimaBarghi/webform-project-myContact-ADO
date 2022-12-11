e6d11012a941dd405186e6108eb7d7ce0923833f
unity <User@User9.iau.org>
2022-12-11 14:24:05 +0330

    first commit

---
+++ MyContact/Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace MyContact
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dgContacts = new System.Windows.Forms.DataGridView();
+            this.ContactID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.MyName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Family = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Mobail = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Email = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Age = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Address = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnNewContacts = new System.Windows.Forms.ToolStripButton();
+            this.btnRefresh = new System.Windows.Forms.ToolStripButton();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.toolStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgContacts)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnNewContacts,
+            this.btnRefresh});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(782, 27);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtSearch);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 39);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(758, 100);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "جستجو";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dgContacts);
+            this.groupBox2.Location = new System.Drawing.Point(12, 166);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(758, 375);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "لیست";
+            // 
+            // dgContacts
+            // 
+            this.dgContacts.AllowUserToAddRows = false;
+            this.dgContacts.AllowUserToDeleteRows = false;
+            this.dgContacts.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgContacts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgContacts.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ContactID,
+            this.MyName,
+            this.Family,
+            this.Mobail,
+            this.Email,
+            this.Age,
+            this.Address});
+            this.dgContacts.Location = new System.Drawing.Point(6, 22);
+            this.dgContacts.Name = "dgContacts";
+            this.dgContacts.ReadOnly = true;
+            this.dgContacts.RowHeadersWidth = 51;
+            this.dgContacts.RowTemplate.Height = 24;
+            this.dgContacts.Size = new System.Drawing.Size(746, 347);
+            this.dgContacts.TabIndex = 0;
+            // 
+            // ContactID
+            // 
+            this.ContactID.DataPropertyName = "ContactID";
+            this.ContactID.HeaderText = "کد";
+            this.ContactID.MinimumWidth = 6;
+            this.ContactID.Name = "ContactID";
+            this.ContactID.ReadOnly = true;
+            // 
+            // MyName
+            // 
+            this.MyName.DataPropertyName = "Name";
+            this.MyName.HeaderText = "نام";
+            this.MyName.MinimumWidth = 6;
+            this.MyName.Name = "MyName";
+            this.MyName.ReadOnly = true;
+            // 
+            // Family
+            // 
+            this.Family.DataPropertyName = "Family";
+            this.Family.HeaderText = "نام خانوادگی";
+            this.Family.MinimumWidth = 6;
+            this.Family.Name = "Family";
+            this.Family.ReadOnly = true;
+            // 
+            // Mobail
+            // 
+            this.Mobail.DataPropertyName = "Mobail";
+            this.Mobail.HeaderText = "موبایل";
+            this.Mobail.MinimumWidth = 6;
+            this.Mobail.Name = "Mobail";
+            this.Mobail.ReadOnly = true;
+            // 
+            // Email
+            // 
+            this.Email.DataPropertyName = "Email";
+            this.Email.HeaderText = "ایمیل";
+            this.Email.MinimumWidth = 6;
+            this.Email.Name = "Email";
+            this.Email.ReadOnly = true;
+            // 
+            // Age
+            // 
+            this.Age.DataPropertyName = "Age";
+            this.Age.HeaderText = "سن";
+            this.Age.MinimumWidth = 6;
+            this.Age.Name = "Age";
+            this.Age.ReadOnly = true;
+            // 
+            // Address
+            // 
+            this.Address.DataPropertyName = "Address";
+            this.Address.HeaderText = "آدرس";
+            this.Address.MinimumWidth = 6;
+            this.Address.Name = "Address";
+            this.Address.ReadOnly = true;
+            // 
+            // btnNewContacts
+            // 
+            this.btnNewContacts.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnNewContacts.Image = ((System.Drawing.Image)(resources.GetObject("btnNewContacts.Image")));
+            this.btnNewContacts.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnNewContacts.Name = "btnNewContacts";
+            this.btnNewContacts.Size = new System.Drawing.Size(136, 24);
+            this.btnNewContacts.Text = "افزودن شخص جدید";
+            this.btnNewContacts.Click += new System.EventHandler(this.btnNewContacts_Click);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.btnRefresh.Image = ((System.Drawing.Image)(resources.GetObject("btnRefresh.Image")));
+            this.btnRefresh.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(76, 24);
+            this.btnRefresh.Text = "بروزرسانی";
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(656, 560);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(108, 28);
+            this.btnEdit.TabIndex = 3;
+            this.btnEdit.Text = "ویرایش";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(500, 560);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(108, 28);
+            this.btnDelete.TabIndex = 4;
+            this.btnDelete.Text = "حذف";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(668, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "جستجو: ";
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(408, 42);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(254, 23);
+            this.txtSearch.TabIndex = 1;
+            this.txtSearch.TextChanged += new System.EventHandler(this.txtSearch_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(782, 600);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.toolStrip1);
+            this.Font = new System.Drawing.Font("Tahoma", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "دفترچه تلفن من";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgContacts)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.DataGridView dgContacts;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ContactID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MyName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Family;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Mobail;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Email;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Age;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Address;
+        private System.Windows.Forms.ToolStripButton btnNewContacts;
+        private System.Windows.Forms.ToolStripButton btnRefresh;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ MyContact/Service/ContactsRepository.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace MyContact
+{
+    class ContactsRepository : IContactsRepository
+    {
+        private string connectionstring = "Data Source=.;Initial Catalog=Contact_DB;Integrated Security=true";
+        public bool Delete(int contactId)
+        {
+            SqlConnection connection = new SqlConnection(connectionstring);
+            try
+            {
+                string query = "Delete From MYContact2 Where ContactID=@ID ";
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@ID", contactId);
+                connection.Open();
+                command.ExecuteNonQuery();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        public bool Insert(string name, string family, string mobile, string email, int age, string address)
+        {
+            SqlConnection connection = new SqlConnection(connectionstring);
+            try
+            {
+                string query = "Insert Into MYContact2 (Name,Family,Mobail,Email,Age,Address) values (@Name,@Family,@Mobail,@Email,@Age,@Address)";
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@Name", name);
+                command.Parameters.AddWithValue("@Family", family);
+                command.Parameters.AddWithValue("@Mobail", mobile);
+                command.Parameters.AddWithValue("@Email", email);
+                command.Parameters.AddWithValue("@Age", age);
+                command.Parameters.AddWithValue("@Address", address);
+                connection.Open();
+                command.ExecuteNonQuery();
+                return true;
+
+            }
+            catch
+            {
+                return false;
+            }
+            finally
+            {
+                connection.Close();
+
+            }
+        }
+
+        public DataTable Search(string Parameter)
+        {
+            string query = "Select * From MYContact2  Where Name like @parameter Or Family like @parameter";
+            SqlConnection conection = new SqlConnection(connectionstring);
+            SqlDataAdapter adaptor = new SqlDataAdapter(query, conection);
+            adaptor.SelectCommand.Parameters.AddWithValue("@parameter", "%" + Parameter + "%");
+            DataTable data = new DataTable();
+            adaptor.Fill(data);
+            return data;
+        }
+
+        public DataTable SelectAll()
+        {
+            string query = "Select * From MYContact2";
+            SqlConnection conection = new SqlConnection(connectionstring);
+            SqlDataAdapter adaptor = new SqlDataAdapter(query,conection);
+            DataTable data = new DataTable();
+            adaptor.Fill(data);
+            return data;
+
+
+        }
+
+        
+        public DataTable SelectRow(int contactId)
+        {
+            string query = "Select * From MYContact2 where ContactID=" + contactId;
+            SqlConnection conection = new SqlConnection(connectionstring);
+            SqlDataAdapter adaptor = new SqlDataAdapter(query, conection);
+            DataTable data = new DataTable();
+            adaptor.Fill(data);
+            return data;
+            
+        }
+
+        public bool Update(int contactId, string name, string family, string Mobail, string email, int age, string address)
+        {
+            SqlConnection connection = new SqlConnection(connectionstring);
+            try
+            {
+                string query = "Update MYContact2 Set Name=@Name,Family=@Family,Mobail=@Mobail,Email=@Email,Age=@Age,Address=@Address Where ContactID=@ID"; 
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@ID", contactId);
+                command.Parameters.AddWithValue("@Name", name);
+                command.Parameters.AddWithValue("@Family", family);
+                command.Parameters.AddWithValue("@Mobail", Mobail);
+                command.Parameters.AddWithValue("@Email", email);
+                command.Parameters.AddWithValue("@Age", age);
+                command.Parameters.AddWithValue("@Address", address);
+                connection.Open();
+                command.ExecuteNonQuery();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+    }
+}
